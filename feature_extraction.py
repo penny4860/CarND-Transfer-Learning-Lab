ee437df89c84dded291deba905b27cadd88d8f70
jeongjoonsup <penny4860@gmail.com>
2017-08-08 17:51:54 +0900

    vgg / inception / resnet 의 실험결과

--- feature_extraction.py
+++ feature_extraction.py
@@ -1,13 +1,27 @@
 import pickle
 import tensorflow as tf
 # TODO: import Keras layers you need here
+from keras.layers import Input, Flatten, Dense
+from keras.models import Model
+
 
 flags = tf.app.flags
 FLAGS = flags.FLAGS
 
 # command line flags
-flags.DEFINE_string('training_file', '', "Bottleneck features training file (.p)")
-flags.DEFINE_string('validation_file', '', "Bottleneck features validation file (.p)")
+flags.DEFINE_string('training_file',
+                    'resnet_cifar10_100_bottleneck_features_train.p',
+                    "Bottleneck features training file (.p)")
+flags.DEFINE_string('validation_file',
+                    'resnet_cifar10_bottleneck_features_validation.p',
+                    "Bottleneck features validation file (.p)")
+flags.DEFINE_integer('epochs',
+                    50,
+                    "number of epoches")
+flags.DEFINE_integer('batch_size',
+                    32,
+                    "batch size")
+
 
 
 def load_bottleneck_data(training_file, validation_file):
@@ -35,21 +49,38 @@ def load_bottleneck_data(training_file, validation_file):
 
 
 def main(_):
+    import numpy as np
     # load bottleneck data
     X_train, y_train, X_val, y_val = load_bottleneck_data(FLAGS.training_file, FLAGS.validation_file)
 
     print(X_train.shape, y_train.shape)
     print(X_val.shape, y_val.shape)
 
-    # TODO: define your model and hyperparams here
-    # make sure to adjust the number of classes based on
-    # the dataset
-    # 10 for cifar10
-    # 43 for traffic
+    nb_classes = len(np.unique(y_train))
 
-    # TODO: train your model here
+    # define model
+    input_shape = X_train.shape[1:]
+    inp = Input(shape=input_shape)
+    x = Flatten()(inp)
+    x = Dense(nb_classes, activation='softmax')(x)
+    model = Model(inp, x)
+    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+    # train model
+    model.fit(X_train, y_train, nb_epoch=FLAGS.epochs, batch_size=FLAGS.batch_size, validation_data=(X_val, y_val), shuffle=True)
 
 
 # parses flags and calls the `main` function above
 if __name__ == '__main__':
+    from keras.datasets import cifar10
+    from sklearn.model_selection import train_test_split
+    
+    
+    # VGG : loss: 0.0308 - acc: 1.0000 - val_loss: 0.9029 - val_acc: 0.7463
+    # Inceltion : loss: 0.0129 - acc: 1.0000 - val_loss: 1.1957 - val_acc: 0.6625
+    # resnet : loss: 0.0102 - acc: 1.0000 - val_loss: 0.8966 - val_acc: 0.7361
     tf.app.run()
+
+
+
+
